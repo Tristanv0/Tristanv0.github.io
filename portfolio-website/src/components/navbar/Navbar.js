84e94385d6f5527274156b94ca2cf27c516593a3
Tristanv0 <cheng.tristan@hotmail.com>
2024-01-05 22:37:56 -0500

    created different event on each hypertext

--- portfolio-website/src/components/navbar/Navbar.js
+++ portfolio-website/src/components/navbar/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Navbar.css';
 
-const Navbar = ({ onClick }) => {
+const Navbar = ({ onHomeClick, onResumeClick, onAboutClick, onProjectsClick, onContactClick }) => {
     return (
         <nav>
             <ul>
-                <li><a href='/' onClick={(event) => onClick(event)}>Home</a></li>
-                <li><a href='/resume' onClick={(event) => onClick(event)}>Resume</a></li>
-                <li><a href='/about' onClick={(event) => onClick(event)}>About</a></li>
-                <li><a href='/projects' onClick={(event) => onClick(event)}>Projects</a></li>
-                <li><a href='/contact' onClick={(event) => onClick(event)}>Contact</a></li>
+                <li><a href='/' onClick={onHomeClick}>Home</a></li>
+                <li><a href='/about' onClick={onAboutClick}>About</a></li>
+                <li><a href='/resume' onClick={onResumeClick}>Resume</a></li>
+                <li><a href='/projects' onClick={onProjectsClick}>Projects</a></li>
+                <li><a href='/contact' onClick={onContactClick}>Contact</a></li>
             </ul>
         </nav>
     );
