e2d75b63f64503e84b5ad8498a103b473c51631b
Tristanv0 <cheng.tristan@hotmail.com>
2024-01-06 23:21:55 -0500

    Added Contact section UI/UX

--- portfolio-website/src/components/contact/Contact.js
+++ portfolio-website/src/components/contact/Contact.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
 
 const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // For demonstration purposes, log the form data to the console
+        console.log('Form Data:', formData);
+        // You can add logic here to send the form data to a server or perform other actions
+    };
+
     return (
         <div className="ContactPage">
-            <h1>This is the Contact section</h1>
+
+                <div className="container-title">
+                    <p className="contact-title">Contact Me</p>
+                </div>
+                <div className="container-form">
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="name">Name:</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            required
+                        />
+
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+
+                        <label htmlFor="message">Message:</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            required
+                        ></textarea>
+
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+
         </div>
     );
 };
